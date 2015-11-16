b30ffbab3868170e0bd0e85c64a8ef4d41aca06e
dc004 <dc004@quanjungang.local>
2015-11-16 09:10:03 +0800

    Initial Commit

---
+++ 穷游/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  穷游
+//
+//  Created by dc004 on 15/11/16.
+//  Copyright © 2015年 gang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 穷游/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  穷游
+//
+//  Created by dc004 on 15/11/16.
+//  Copyright © 2015年 gang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
